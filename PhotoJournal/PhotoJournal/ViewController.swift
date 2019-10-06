5af3a450c1dc4bc38e3762c7863f45ff933b6797
Kimball Yang <kimball.yang@gmail.com>
2019-10-06 14:33:37 -0400

    Added outlets and working on Add function

--- PhotoJournal/PhotoJournal/ViewController.swift
+++ PhotoJournal/PhotoJournal/ViewController.swift
@@ -9,16 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-
+    
+    
     @IBOutlet weak var photoCollection: UICollectionView!
     
- 
+    
     @IBAction func optionClick(_ sender: Any) {
     }
     
     
     @IBAction func add(_ sender: Any) {
+        
+        let storyboard = UIStoryboard.init(name: "Main", bundle: nil)
+        guard let VC = storyboard.instantiateViewController(identifier: "editor") as? EditorViewController
+            else {
+                fatalError()
+        }
+        VC.modalPresentationStyle = .currentContext
+        present(VC, animated: true, completion: nil)
+        
+        
     }
     
     
@@ -27,7 +37,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
 }
 
